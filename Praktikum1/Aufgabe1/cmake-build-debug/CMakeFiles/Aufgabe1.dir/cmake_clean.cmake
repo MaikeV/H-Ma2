9f881f8d126723181a20fb3d4f2c5090214b8ad8
Maike <maike.voss7@gmail.com>
2019-04-08 10:41:54 +0200

    Fixed Folder Structure

---
+++ Praktikum1/Aufgabe1/cmake-build-debug/CMakeFiles/Aufgabe1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aufgabe1.dir/main.cpp.obj"
+  "Aufgabe1.pdb"
+  "Aufgabe1.exe"
+  "Aufgabe1.exe.manifest"
+  "libAufgabe1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aufgabe1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
