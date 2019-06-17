fa5d46e97d125817549d254ccab79c7b8b02585b
Maike <maike.voss7@gmail.com>
2019-06-17 11:07:17 +0200

    Praktikum4

---
+++ Praktikum4/CKomplex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mauske on 16.06.19.
+//
+
+#include "CKomplex.h"
---
+++ Praktikum4/CKomplex.h
@@ -0,0 +1,16 @@
+//
+// Created by mauske on 16.06.19.
+//
+
+#ifndef PRAKTIKUM4_CKOMPLEX_H
+#define PRAKTIKUM4_CKOMPLEX_H
+
+
+
+class CKomplex {
+
+};
+
+
+
+#endif //PRAKTIKUM4_CKOMPLEX_H
